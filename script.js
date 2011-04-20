b9214b86b823f2220c35ae581a4230f173e22225
Roberto Torella <robertot@square-enix.com>
2011-04-20 17:05:36 +0100

    added camera and world - still working on perspective projection

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 $(document).ready(function(){
-	var o=new Obj();
+	var o1=new Obj();
+	var o2=new Obj();
+	var o3=new Obj();
 	
-	o.addPoint([-1,-1,-1]).
-		addPoint([-1, 1,-1]).
-		addPoint([-1,-1, 1]).
-		addPoint([ 1,-1,-1]).
-		transform().
-			translate(1,2,3).
-			rotateX("30Deg").
-			translate(-1,-2,-3).
-			rotateY("-30deg");
+	o1.
+		addPoint([-1, 0,-1]).
+		addPoint([ 1, 0,-1]).
+		addPoint([ 1, 0, 1]).
+		addPoint([-1, 0, 1]);
 		
-	console.log(o.points());
+	o2.
+		addPoint([0, -1,-1]).
+		addPoint([0,  1,-1]).
+		addPoint([0,  1, 1]).
+		addPoint([0, -1, 1]);
+		
+	o3.
+		addPoint([-1,-1, 0]).
+		addPoint([ 1,-1, 0]).
+		addPoint([ 1, 1, 0]).
+		addPoint([-1, 1, 0]);
+		
+	var pos=new Vector(0,0,20);
+	var look_at=new Vector(0,0,0);
+	var c=new Camera(pos,look_at,0,10);
+	
+	var w=new World();
+	
+	w.addObject(o1).addObject(o2).addObject(o3).addCamera(c).render();
 });
